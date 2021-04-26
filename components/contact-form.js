1ba4974ecbb49ebc832f85073cfdc4ca8fa02350
ny-bin <ny.bin1210@gmail.com>
2021-04-26 15:24:28 +0900

    🍱 create contactform

---
+++ components/contact-form.js
@@ -0,0 +1,23 @@
+export function ContactForm() {
+  return (
+    <div>
+      <p className="relative py-12 text-5xl text-center text-white">Contact Me</p>
+      <p className="text-lg leading-loose text-center text-gray-300 py-5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam.
+      </p>
+      <p className="left-0 bottom-0 text-sm leading-snug text-gray-300 py-5">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+        aspernatur aut.
+      </p>
+      <p className=" right-0 bottom-0 text-sm leading-snug text-gray-300 py-5">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+        aspernatur aut.
+      </p>
+    </div>
+  );
+}
